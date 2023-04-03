692b37594934b44ba145b366d7b5cffa2866d276
EwoutvanderVelde <ewoutvandervelde@gmail.com>
2023-04-03 11:24:01 +0200

    cleaner

--- Streamlit/colabborative_reccomendations.py
+++ Streamlit/colabborative_reccomendations.py
@@ -7,121 +7,208 @@ import pandas as pd
 import numpy as np
 import pickle
 import streamlit as st
+DEBUG = True
 
 
-# Function to open pickle files, we will open a lot of them...
+
 def open_pickle(varname):
+    """
+    Function to open pickle files, we will open a lot of them...
+    """
     with open(f'data/{varname}.pkl', 'rb') as fp:
         var = pickle.load(fp)
     return var
 
-# Load the activities data again to make sure we have the latest data.
+
 def renew_data_frames():
+    """
+    Load the activities data again to make sure we have the latest data.
+    """
     df_activities = pd.read_json("data/activities.json")
     df_ratings = df_activities[df_activities['rating'] != "no rating"]
+
+    if DEBUG:
+        print(f"collabFilter.py renew_data_drames df_ratings: {df_ratings}")
+    
     return df_ratings
 
-def get_user_row_from_df(df_selected:pd.DataFrame, userID:str)-> list[int]:
-        return list(df_selected[df_selected['user_id'] == userID]['rating'].astype(np.float32))
 
-def create_user_rating_row(df_selected, mediaIDList, index_userID, mediaID_index, userID):
-    cols = (len(mediaIDList))
-    user_ratings=np.zeros([1,cols], dtype=np.int8)
-    for i, row in enumerate(user_ratings):  # For each user
-        useractivities = df_selected[df_selected['user_id'] == userID]  
-        for j in useractivities.iterrows():
-            content_id = (j[1]["content_id"])
-            columnindex = mediaID_index[content_id]
+def get_user_ratings_from_df(df_ratings:pd.DataFrame, userID:str)-> list[int]:
+    """
+    Get all the ratings from the current user in list format
+    """
+    if DEBUG:
+        print(f"collabFilter.py get_user_ratings_from_df df_ratings[df_ratings['user_id'] == userID]['rating']: {df_ratings[df_ratings['user_id'] == userID]['rating']}")
+    
+    return df_ratings[df_ratings['user_id'] == userID]
+
+def get_mean_user_ratings(df_ratings:pd.DataFrame, userID:str)-> list[int]:
+    """
+    Get all the ratings from the current user in list format
+    """
+    mean = np.mean(list(get_user_ratings_from_df(df_ratings, userID)['rating'].astype(np.float32)))
+    if DEBUG:
+        print(f"collabFilter.py get_mean_user_ratings mean: {mean}")
+
+    return mean
+
+
+def create_user_rating_row(df_ratings, mediaIDList, mediaID2index, userID):
+    """
+    Get the user rating in matrix form
+    Since we are looking at 1 user, this is of the for 1 x len(unique_media)
+    Row is needed to calculate simularity to other users.
+    """
+    ncols = (len(mediaIDList))
+    own_ratings=np.zeros([1,ncols], dtype=np.int8)  # np.int8 is to save some memory
+    
+    if DEBUG:
+        print(f"collabFilter.py create_user_rating_row user_ratings: {own_ratings}")
+        print(f"collabFilter.py create_user_rating_row type(user_ratings): {type(own_ratings)}")
+        print(f"collabFilter.py create_user_rating_row user_ratings.shape: {own_ratings.shape}")
+
+    for i, row in enumerate(own_ratings):  # For each user
+        current_user_ratings = df_ratings[df_ratings['user_id'] == userID]  
+        for j in current_user_ratings.iterrows():
+            mediaID = (j[1]["content_id"])
+            columnindex = mediaID2index[mediaID]
             row[columnindex] = int(j[1]["rating"])
-    return user_ratings
+    
+    if DEBUG:
+        print(f"collabFilter.py create_user_rating_row user_ratings_filled: {own_ratings}")
+        print(f"collabFilter.py create_user_rating_row type(user_ratings_filled): {type(own_ratings)}")
+        print(f"collabFilter.py create_user_rating_row user_ratings_filled.shape: {own_ratings.shape}")
+    
+    return own_ratings[0]
 
 
-def get_simularity_to_other_users(user_rating_matrix, user_mean_dict, userID, index_userID, userID_index, df_selected, mediaIDList, mediaID_index):
-    #index_a does not exist normaly, except if user 
+def get_simularity_to_other_users(user_rating_matrix, user_mean_dict, userID, index2userID, userID2index, df_ratings, mediaIDList, mediaID2index):
+    """
+    Calculate how simulair our user is to other users in the database that we have from other users
+    """
+    
+    # If current user has not made any recommendations yes, we can not find him in our dict
     try:
-        index_a = userID_index[userID]
+        index_current_user = userID2index[userID]
     except:
-        index_a = -1
+        index_current_user = -1
     
-    row_a = create_user_rating_row(df_selected, mediaIDList, index_userID, mediaID_index, userID)[0]
+    row_current_user = create_user_rating_row(df_ratings, mediaIDList, mediaID2index, userID)
+
+    if DEBUG:
+        print(f"collabFilter.py get_simularity_to_other_users row_current_user: {row_current_user}")
+        print(f"collabFilter.py get_simularity_to_other_users type(row_current_user): {type(row_current_user)}")
+        print(f"collabFilter.py get_simularity_to_other_users row_current_user.shape: {row_current_user.shape}")
+
+    # We are gonna substract the mean of eacht user from their ratings, 
+    # if a user only has 1 rating, their new score would become 0, this can become problamatic.
+    # Of user only has one rating, set their mean to 0
+    if len(row_current_user[row_current_user != 0]) >1: 
+        row_current_user_mean = get_mean_user_ratings(df_ratings, userID)
+    else:
+        row_current_user_mean = 0
+
+    row_current_user_normalized = [score-row_current_user_mean if score !=0 else 0 for score in row_current_user]
+
     simularity_dict = {}
-    
-    if len(row_a[row_a != 0]) >1: # Avoid devision by zero lateron
-        row_a_mean = np.mean(get_user_row_from_df(df_selected, userID))
-    else:
-        row_a_mean = 0
-    row_a_norm = [score-row_a_mean if score !=0 else 0 for score in row_a]
 
-    for index_b, row_b in enumerate(user_rating_matrix):
-        if index_a != index_b:
-            row_b_mean = user_mean_dict[index_userID[index_b]]
-            if len(row_b[row_b != 0] >1): # Avoid devision by zero lateron
-                row_b_norm = [score-row_b_mean if score !=0 else 0 for score in row_b]
+    for index_other_user, row_other_user in enumerate(user_rating_matrix):
+        if index_current_user != index_other_user:
+            row_other_user_mean = user_mean_dict[index2userID[index_other_user]]
+            
+            # ckeck if other users have more than 1 rating
+            if len(row_other_user[row_other_user != 0] >1): # Avoid devision by zero lateron
+                row_other_user_normalized = [score-row_other_user_mean if score !=0 else 0 for score in row_other_user]
             else:
-                row_b_norm = row_b
-            numerator = np.dot(row_a_norm, row_b_norm)
-            denomenator = (np.linalg.norm(row_a_norm) * np.linalg.norm(row_b_norm))
+                row_other_user_normalized = row_other_user
+
+            numerator = np.dot(row_current_user_normalized, row_other_user_normalized)
+            denomenator = (np.linalg.norm(row_current_user_normalized) * np.linalg.norm(row_other_user_normalized))
+            
+            # We still sometimes get a division by zero, not sure why
             if denomenator != 0:
-                sim = numerator/denomenator
+                simularity = numerator/denomenator
             else: 
-                sim=0   
-            simularity_dict[index_userID[index_b]] = sim
-    return simularity_dict, row_a_mean
+                simularity=0
+
+            simularity_dict[index2userID[index_other_user]] = simularity
+    return simularity_dict, row_current_user_mean
 
 
-def sort_simularitys(simularitys:dict)->dict:
-    return dict(sorted(simularitys.items(), key=lambda x:x[1]))
+def sort_simularities(simularities:dict)->dict:
+    """
+    Name is somewhat self explenetory
+    """
+    return dict(sorted(simularities.items(), key=lambda x:x[1]))
 
 
-def userItemPrediction(df_selected, simularity_to_other_users, current_user_mean, user_rating_matrix, mediaID_index, index_mediaID, userID_index, max_neighbourhood = 50):
-    sorted_simularitys = sort_simularitys(simularity_to_other_users)
-    
-    if len(sorted_simularitys) < max_neighbourhood:
-        neighbours = sorted_simularitys
+def userItemPrediction(df_selected, simularity_to_other_users, current_user_mean, user_rating_matrix, mediaID2index, userID2index, max_neighbourhood = 50)->dict:
+    """
+    Predict rating for current user
+    """
+
+    # Get the n most simulair users (neighbours)
+    sorted_simularities = sort_simularities(simularity_to_other_users)
+    if len(sorted_simularities) < max_neighbourhood:
+        neighbours = sorted_simularities
     else:
-        neighbours = sorted_simularitys[:max_neighbourhood]
+        neighbours = sorted_simularities[:max_neighbourhood]
+
+    # We only want to make predictions for which we know a rating from other users
     unique_mediaID = df_selected[df_selected['user_id'].isin(neighbours.keys())]["content_id"].unique()
+
+    # Use the formula on slides week 6
     recommendations = {}
     for mediaID in unique_mediaID:
-        matrix_mediaID_index = mediaID_index[mediaID]
+        matrix_mediaID_index = mediaID2index[mediaID]
         numerator = 0
         denominator = 0        
         for userID, simularity in neighbours.items():
-            if((user_rating:= user_rating_matrix[userID_index[userID], matrix_mediaID_index]) != np.nan):
+            if((user_rating:= user_rating_matrix[userID2index[userID], matrix_mediaID_index]) != np.nan):
                 numerator += simularity*user_rating
                 denominator += simularity
         recommendations[mediaID] = current_user_mean + (numerator/denominator)
     return recommendations
 
 
-def renewPersonalRecomedations(current_user, df_npo):
+def renewPersonalRecomedations(current_user, df_npo)->pd.DataFrame:
+    """
+    Function called from streamlit button. Will return a dataframe with shows that simular users watch.
+    """
     # Load all pre processed data
-    df_selected = renew_data_frames()
+    df_ratings = renew_data_frames()
     userlist = open_pickle("userlist")
     mediaIDList = open_pickle("mediaIDList")
-    userID_index, index_userID = open_pickle("userID_index"), open_pickle("index_userID")
-    mediaID_index, index_mediaID = open_pickle("mediaID_index"), open_pickle("index_mediaID")
+    userID2index, index2userID = open_pickle("userID_index"), open_pickle("index_userID")
+    mediaID2index, index2mediaID = open_pickle("mediaID_index"), open_pickle("index_mediaID")
     user_rating_matrix = np.load('data/user_rating_matrix.npy')
     user_rating_mean = open_pickle("user_rating_mean")
     
-    simularity_to_other_users, current_user_mean = get_simularity_to_other_users(user_rating_matrix, user_rating_mean, current_user, index_userID, userID_index, df_selected, mediaIDList, mediaID_index)
-    predictions = userItemPrediction(df_selected, simularity_to_other_users, current_user_mean, user_rating_matrix, mediaID_index, index_mediaID, userID_index)
-    # nog kiezen welke handiger is...
+    simularity_to_other_users, current_user_mean = get_simularity_to_other_users(user_rating_matrix, user_rating_mean, current_user, index2userID, userID2index, df_ratings, mediaIDList, mediaID2index)
+    predictions = userItemPrediction(df_ratings, simularity_to_other_users, current_user_mean, user_rating_matrix, mediaID2index, userID2index)
+    
+    # Get the dataframe entries from our recommendations and add prediction score
     result = df_npo[df_npo["mediaID"].isin(predictions.keys())]
     result['userPrediction'] = result['mediaID'].apply(lambda x: predictions[x])
-    #st.session_state['userRecommendations'] = df_npo[df_npo["mediaID"].isin(predictions.keys())]
     st.session_state['userRecommendations'] = result
 
     return result
 
 
+
 def test(df_npo):
+    """
+    Function used to test the script without using streamlit
+    """
     df_selected = renew_data_frames()
     print(f'-------------------------RESULT --------------------------------- \n {renewPersonalRecomedations("zuiiztxxjt",df_npo)}')
     return
 
-#test(pd.read_csv("data/NPOPlayerv2.csv"))
-#renewPersonalRecomedations("teqocdjxcd", renew_data_frames())
+
+# Only run when we are debugging, this does not run when script is imported
+if __name__ == "__main__":
+    # Calling functions to test
+    test(pd.read_csv("data/NPOPlayerv2.csv"))
 
 
 
--- Streamlit/liveCalc.py
+++ Streamlit/liveCalc.py
@@ -8,13 +8,18 @@ import streamlit as st
 with open('data/miediaID_NERTags.json', 'r') as fp:
     media_tag_dict = json.load(fp)
 """ 
-# Used to generate a random string as an "unique" identifier for the user
+
 def generate_random_userID(length):
+    """
+    Generate a rondom string of length n as an "unique" identifier for the user
+    """
     return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
 
-# standard jaccard_similarity function
 def jaccard_similarity(mediaID_tags_a:list[str], mediaID_tags_b:list[str])->float:
+    """
+    Standard jaccard simularity function for lists of string (but should word with numbers)
+    """
     set_tags_a = set(mediaID_tags_a)
     set_tags_b = set(mediaID_tags_b)
     
@@ -36,21 +41,28 @@ def get_jaccard_similarity_list(mediaID:str)->list[list[str, float]]:
     return distancelist
 """
 
-# filter function to even out the recommendations. Use this function to get only n recommendations per broadcaster
 def max_n_reccomendation_per_broadcaster(df:pd.DataFrame, max_n:int = 2)->pd.DataFrame:
-    broadcastcount = {}
+    """
+    Filter function to smooth the recommendations. Use this function to get only n recommendations per broadcaster
+    """
+    broadcastcounter = {}
     def check_if_double(broadcaster:str):
-        if broadcaster not in broadcastcount.keys():
-            broadcastcount[broadcaster] = 1
+        if broadcaster not in broadcastcounter.keys():
+            broadcastcounter[broadcaster] = 1
             return True
-        elif broadcastcount[broadcaster] < max_n:
-            broadcastcount[broadcaster] += 1
+        elif broadcastcounter[broadcaster] < max_n:
+            broadcastcounter[broadcaster] += 1
             return True
         else:
             return False
-    df_copy = df.copy()  # Make a copy, since in the next step we create a column, and we do not want to modify the original one
-    df_copy["keep"] = df_copy["broadcaster"].apply(lambda x: check_if_double(x))            
-    return df_copy[df_copy["keep"]==True].drop(columns= ["keep"])  # Drop the keep column for consistency
+        
+    # We make a copy just in case
+    df_copy = df.copy()
+    df_copy["keep"] = df_copy["broadcaster"].apply(lambda x: check_if_double(x))    
+    
+    # Drop the keep column for consistency
+    result = df_copy[df_copy["keep"]==True].drop(columns= ["keep"])
+    return result 
 
 """ Depreciated
 def get_top_k_ner_jaccard(df:pd.DataFrame, mediaID:str, topk:int=10, exclude_current_broadcaster=True, no_repeat=True)-> pd.DataFrame:
@@ -71,21 +83,32 @@ def get_top_k_ner_jaccard(df:pd.DataFrame, mediaID:str, topk:int=10, exclude_cur
 """
 
 def get_similar_content(df, mediaID):
+    """
+    returns dataframe where content is has the same topic as mediaID
+    """
     topic = df[df['mediaID'] == mediaID]['Topic'].values[0]
     return df[df['Topic'] == topic]
 
+
 def getRecommendations(df:pd.DataFrame, mediaID:str, topk:int=10, exclude_current_broadcaster=True, no_repeat=True)->pd.DataFrame:
+    """
+    Return a dataframe with recommendations. Depending if the user has already rated content,
+    it will either return content based recommendations, or a combination of content and collaberative based recommendations.
+    """
+    
     content_based_recommendations = get_similar_content(df, mediaID)
     content_based_recommendations = content_based_recommendations[content_based_recommendations["mediaID"] != mediaID]
+    
     user_based_recommendations = st.session_state['userRecommendations']
+    
     if user_based_recommendations is None:
         return max_n_reccomendation_per_broadcaster(content_based_recommendations).sample(topk, replace=False)
     else:
         in_both = pd.merge(content_based_recommendations, user_based_recommendations[["mediaID", "userPrediction"]]).sort_values('userPrediction', ascending= False)
         if (len(in_both) < topk):
             n_to_add = topk - len(in_both)
-            in_both = in_both.append(user_based_recommendations[user_based_recommendations["mediaID"] != mediaID])
+            in_both = pd.concat([in_both, user_based_recommendations[user_based_recommendations["mediaID"] != mediaID]], axis = 0, ignore_index=True)
+            #in_both = in_both.append(user_based_recommendations[user_based_recommendations["mediaID"] != mediaID])
 
         result = max_n_reccomendation_per_broadcaster(in_both, max_n=2).head(topk)
-
         return result
